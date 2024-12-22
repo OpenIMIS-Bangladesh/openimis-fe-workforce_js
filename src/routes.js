5456d18465d6c1dceb44dc6aa92570443825998b
theeMahmud <mahmudrajucse@gmail.com>
2024-12-22 15:46:05 +0600

    organization create / edit from url fixation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 export const ROUTE_WORKFORCE_ORGANIZATIONS = "workforce/organizations";
-export const ROUTE_WORKFORCE_ORGANIZATIONS_CREATE = "workforce/organizations/create";
 
 export const ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION = "workforce/organizations/organization";
 
