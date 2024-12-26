b700ec73d1b0e4e812c9002bb4c39b1c94b30d30
theeMahmud <mahmudrajucse@gmail.com>
2024-12-26 17:32:26 +0600

    clean

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import messages_en from "./translations/en.json";
 import WorkforceMainMenu from "./menu/WorkforceMainMenu";
-import reducer from './reducer';
+import reducer from "./reducer";
 
 
 import {
@@ -8,32 +8,35 @@ import {
   ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION,
   ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION_EDIT,
   ROUTE_WORKFORCE_ORGANIZATIONS_UNITS,
-  ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT
+  ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT,
 } from "./routes";
 import WorkforceOrganizationsPage from "./pages/WorkforceOranigzationsPage";
 import WorkforceOrganizationPage from "./pages/WorkforceOrganizationPage";
-import WorkforceOranigzationsUnitPage from "./pages/WorkforceOranigzationsUnitPage";
+import WorkforceOrganizationUnitsPage from "./pages/WorkforceOrganizationUnitsPage";
 import WorkforceOrganizationUnitPage from "./pages/WorkforceOrganizationUnitPage";
 import ParentPicker from "./pickers/ParentPicker";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
-  reducers: [{ key: 'workforce', reducer }],
+  reducers: [{ key: "workforce", reducer }],
 
- refs: [
-    { key: 'workforce.route.organizations.organization', ref: ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION },
-    { key: 'workforce.route.organizations.organization.units.unit', ref: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT },
-    { key: 'workforceOrganization.ParentPicker', ref: ParentPicker },
-    { key: 'workforceOrganization.ParentPicker', ref: ParentPicker },
+  refs: [
+    { key: "workforce.route.organizations.organization", ref: ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION },
+
+    { key: "workforce.route.organizations.organization.units.unit", ref: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT },
+
+    { key: "workforceOrganization.ParentPicker", ref: ParentPicker },
   ],
 
-  'core.Router': [
+  "core.Router": [
     { path: ROUTE_WORKFORCE_ORGANIZATIONS, component: WorkforceOrganizationsPage },
     { path: ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION, component: WorkforceOrganizationPage },
-    { path: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS, component: WorkforceOranigzationsUnitPage },
-    { path: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT, component: WorkforceOrganizationUnitPage },
     { path: `${ROUTE_WORKFORCE_ORGANIZATIONS_ORGANIZATION}/:ticket_uuid`, component: WorkforceOrganizationPage },
+
+    { path: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS, component: WorkforceOrganizationUnitsPage },
+    { path: ROUTE_WORKFORCE_ORGANIZATIONS_UNITS_UNIT, component: WorkforceOrganizationUnitPage },
   ],
+
   "core.MainMenu": [WorkforceMainMenu],
 };
 
