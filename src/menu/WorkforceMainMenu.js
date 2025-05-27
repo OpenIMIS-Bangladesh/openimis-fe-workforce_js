99be9ce45d661ed8916edaeafea7c2b6c07c5997
theeMahmud <mahmudrajucse@gmail.com>
2025-05-27 13:29:23 +0600

    menu reformed

--- src/menu/WorkforceMainMenu.js
+++ src/menu/WorkforceMainMenu.js
@@ -37,36 +37,42 @@ function WorkforceMainMenu(props) {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.organizations" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_ORGANIZATIONS}`,
+      id: `menu.workforce.organizations`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.organizations.unit" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_ORGANIZATIONS_UNITS}`,
+      id: `menu.workforce.organizations.unit`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.organizations.unit.designation" />,
       icon: <ListAlt />,
       route: `/${ROUTE_ORGANIZATIONS_UNIT_DESIGNATIONS}`,
+      id: `menu.workforce.organizations.unit.designation`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.organizations.employee" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_ORGANIZATIONS_EMPLOYEES}`,
+      id: `menu.workforce.organizations.employee`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.organizations.employee.designation" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_ORGANIZATIONS_EMPLOYEES_DESIGNATION}`,
+      id: `menu.workforce.organizations.employee.designation`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.company" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_COMPANIES}`,
+      id: `menu.workforce.companies`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
 
@@ -74,46 +80,54 @@ function WorkforceMainMenu(props) {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.office" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_OFFICES}`,
+      id: `menu.workforce.offices`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.factory" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_FACTORIES}`,
+      id: `menu.workforce.factories`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.approve.company" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_APPROVE_COMPANIES}`,
+      id: `menu.workforce.approve.company`,
       filter: (rights) => rights.includes(RIGHT_WORKFORCE_EMPLOYER_APPROVE),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.employee" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_EMPLOYEES}`,
+      id: `menu.workforce.employees`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.application" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_APPLICATION}`,
+      id: `menu.workforce.application`,
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.application.process" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_APPLICATIONS_PROCESS}`,
+      id: `menu.workforce.application.process`,
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.banks" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_BANKS}`,
+      id: `menu.workforce.banks`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     {
       text: <FormattedMessage module={MODULE_NAME} id="menu.workforce.documents" />,
       icon: <ListAlt />,
       route: `/${ROUTE_WORKFORCE_DOCUMENTS}`,
+      id: `menu.workforce.documents`,
       // filter: (rights) => rights.includes(RIGHT_TICKET_SEARCH),
     },
     // {
@@ -129,6 +143,7 @@ function WorkforceMainMenu(props) {
         {...props}
         header={<FormattedMessage module="workforce" id="menu.workforce" />}
         entries={entries}
+        menuId="WorkforceMainMenu"
       />
     );
   } else {
