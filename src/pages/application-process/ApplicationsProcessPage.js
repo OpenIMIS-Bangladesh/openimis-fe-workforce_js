52f7f94f73a9666c21903e164cb8873107e60be1
theeMahmud <mahmudrajucse@gmail.com>
2025-06-23 16:14:07 +0600

    bftn modal

--- src/pages/application-process/ApplicationsProcessPage.js
+++ src/pages/application-process/ApplicationsProcessPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { connect } from "react-redux";
 import { withTheme, withStyles } from "@material-ui/core/styles";
 import { Fab } from "@material-ui/core";
@@ -9,13 +9,9 @@ import {
 import PrintIcon from '@material-ui/icons/Print';
 import { MODULE_NAME } from "../../constants";
 import ApplicationProcessSearcher from "../../components/application-process/ApplicationProcessSearcher";
-import { ROUTE_WORKFORCE_ORGANIZATIONS_EMPLOYEES_EMPLOYEE } from "../../routes";
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import TabPanel from "./TabPanel";
 import GenerateBFTN from "./GenereteBFTN";
 
@@ -23,25 +19,25 @@ const styles = (theme) => ({
   page: theme.page,
   fab: {
     ...theme.fab,
-    display:"flex",
-    flexDirection:"column"
+    display: "flex",
+    flexDirection: "column"
   },
 });
 
 class ApplicationsProcessPage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      value:props.value || 0,
-      openGenerateBFTN:false,
+      value: props.value || 0,
+      openGenerateBFTN: false,
     }
   }
 
-  handleCloseBFTN = ()=>{
-    this.setState({openGenerateBFTN:false})
+  handleCloseBFTN = () => {
+    this.setState({ openGenerateBFTN: false })
   }
-  handleOpenBFTN = ()=>{
-    this.setState({openGenerateBFTN:true})
+  handleOpenBFTN = () => {
+    this.setState({ openGenerateBFTN: true })
   }
 
   onDoubleClick = (application, newTab = false) => {
@@ -70,71 +66,71 @@ class ApplicationsProcessPage extends Component {
   };
 
   render() {
-    const { intl, classes, rights,applications, } = this.props;
-    const {value,openGenerateBFTN}=this.state;
-console.log("process page",applications)
+    const { intl, classes, rights, applications, } = this.props;
+    const { value, openGenerateBFTN } = this.state;
+    console.log("process page", applications)
     return (
       <div className={classes.page}>
-         <AppBar position="static">
-        <Tabs value={value} onChange={this.handleChange} aria-label="simple tabs example">
-        <Tab
-            label={<FormattedMessage module="workforce" id="workforce.application.process.all" />}
-            {...this.a11yProps(0)}
-          />
-         <Tab
-            label={<FormattedMessage module="workforce" id="workforce.application.process.accidental" />}
-            {...this.a11yProps(1)}
-          />
-          <Tab
-            label={<FormattedMessage module="workforce" id="workforce.application.process.death" />}
-            {...this.a11yProps(2)}
-          />
-          <Tab
-            label={<FormattedMessage module="workforce" id="workforce.application.process.disability" />}
-            {...this.a11yProps(3)}
-          />
-        </Tabs>
-      </AppBar>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange} aria-label="simple tabs example">
+            <Tab
+              label={<FormattedMessage module="workforce" id="workforce.application.process.all" />}
+              {...this.a11yProps(0)}
+            />
+            <Tab
+              label={<FormattedMessage module="workforce" id="workforce.application.process.accidental" />}
+              {...this.a11yProps(1)}
+            />
+            <Tab
+              label={<FormattedMessage module="workforce" id="workforce.application.process.death" />}
+              {...this.a11yProps(2)}
+            />
+            <Tab
+              label={<FormattedMessage module="workforce" id="workforce.application.process.disability" />}
+              {...this.a11yProps(3)}
+            />
+          </Tabs>
+        </AppBar>
 
-      <TabPanel value={value} index={0}>
-        <ApplicationProcessSearcher
-          cacheFiltersKey="allApplications"
-          onDoubleClick={this.onDoubleClick}
-        />
-        <GenerateBFTN  open={openGenerateBFTN} onClose={this.handleCloseBFTN} applications={applications}/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <ApplicationProcessSearcher
-          cacheFiltersKey="accidentalApplications"
-          applicationType="accidentalGrant"
-          onDoubleClick={this.onDoubleClick}
-        />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <ApplicationProcessSearcher
-          cacheFiltersKey="deathApplications"
-          applicationType="deadlyGrant"
-          onDoubleClick={this.onDoubleClick}
-        />
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <ApplicationProcessSearcher
-          cacheFiltersKey="disabilityApplications"
-          applicationType="disabilityAssistance"
-          onDoubleClick={this.onDoubleClick}
-        />
-      </TabPanel>
+        <TabPanel value={value} index={0}>
+          <ApplicationProcessSearcher
+            cacheFiltersKey="allApplications"
+            onDoubleClick={this.onDoubleClick}
+          />
+          <GenerateBFTN open={openGenerateBFTN} onClose={this.handleCloseBFTN} applications={applications} userRights={rights} />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <ApplicationProcessSearcher
+            cacheFiltersKey="accidentalApplications"
+            applicationType="accidentalGrant"
+            onDoubleClick={this.onDoubleClick}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <ApplicationProcessSearcher
+            cacheFiltersKey="deathApplications"
+            applicationType="deadlyGrant"
+            onDoubleClick={this.onDoubleClick}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <ApplicationProcessSearcher
+            cacheFiltersKey="disabilityApplications"
+            applicationType="disabilityAssistance"
+            onDoubleClick={this.onDoubleClick}
+          />
+        </TabPanel>
 
         {withTooltip(
           <div className={classes.fab}>
-            <Fab color="primary" onClick={this.onAdd} style={{marginBottom:10}}>
+            <Fab color="primary" onClick={this.onAdd} style={{ marginBottom: 10 }}>
               <AddIcon />
             </Fab>
             <Fab color="primary" onClick={this.handleOpenBFTN}>
               <PrintIcon />
             </Fab>
           </div>,
-          <FormattedMessage module={MODULE_NAME} id={"workforce.employee.application.addNewTooltip"} />, 
+          <FormattedMessage module={MODULE_NAME} id={"workforce.employee.application.addNewTooltip"} />,
         )}
       </div>
     );
--- src/pages/application-process/GenereteBFTN.js
+++ src/pages/application-process/GenereteBFTN.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   Dialog,
   DialogTitle,
@@ -12,156 +11,156 @@ import {
   Typography,
   Button,
   Divider,
-  IconButton,
 } from '@material-ui/core';
-import { MODULE_NAME, WORKFORCE_USER_TYPE } from "../../constants";
-import { getUserTypeFromRights, isEmptyObject } from "../../utils/utils";
+import { WORKFORCE_USER_TYPE } from "../../constants";
+import { getUserTypeFromRights } from "../../utils/utils";
 import ForwardIcon from "@material-ui/icons/Forward";
 import { WORKFORCE_STATUS } from "../../constants";
-import { fetchApplication, createApplicationSummary } from "../../actions";
-import { useSelector, useDispatch } from "react-redux";
+import { createApplicationSummary } from "../../actions";
+import { useDispatch } from "react-redux";
 
-const GenerateBFTN = ({ open, onClose, applications = [],userRights }) => {
+const GenerateBFTN = ({ open, onClose, applications = [], userRights }) => {
   const getTotalAmount = () => {
     return applications.reduce((sum, item) => sum + (parseFloat(item.approvedAmount) || 0), 0).toFixed(2);
   };
 
-  console.log("generete bftn",applications)
   const dispatch = useDispatch();
-   const handleForward = async () => {
-  
-        const updateApplicationData = {
-          status: WORKFORCE_STATUS.FORWARD_TO_DIRECTOR,
-        };
-        const createApplicationMovementData = {
-          status: WORKFORCE_STATUS.FORWARD_TO_DIRECTOR,
-          note: "আবেদন ডিরেক্টরের কাছে প্রেরণ করা হয়েছে",
-        };
-     await dispatch(
-          createApplicationSummary(
-            updateApplicationData,
-            `create application summary`
-          )
-        );
-     await dispatch(
-          createApplicationSummaryMovement(
-            createApplicationMovementData,
-            `create application summary movement`
-          )
-        );
-        setServerResponse({ status: "SUCCESS", message: "সাবমিশন সফল হয়েছে!" });
-  
+  const handleForward = async () => {
+
+    const updateApplicationData = {
+      status: WORKFORCE_STATUS.FORWARD_TO_DIRECTOR,
     };
-  
+    const createApplicationMovementData = {
+      status: WORKFORCE_STATUS.FORWARD_TO_DIRECTOR,
+      note: "আবেদন ডিরেক্টরের কাছে প্রেরণ করা হয়েছে",
+    };
+    await dispatch(
+      createApplicationSummary(
+        updateApplicationData,
+        `create application summary`
+      )
+    );
+    await dispatch(
+      createApplicationSummaryMovement(
+        createApplicationMovementData,
+        `create application summary movement`
+      )
+    );
+    setServerResponse({ status: "SUCCESS", message: "সাবমিশন সফল হয়েছে!" });
+
+  };
+
   if (getUserTypeFromRights(userRights) === WORKFORCE_USER_TYPE.APPROVER) {
-  return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle disableTypography>
-        <Typography variant="h6">Selected Application Summary</Typography>
-      </DialogTitle>
-      <DialogContent dividers>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Applicant Name</TableCell>
-              <TableCell>Application Type</TableCell>
-              <TableCell align="right">Approved Amount</TableCell>
-              <TableCell>Account No</TableCell>
-              <TableCell>Bank Name</TableCell>
-              <TableCell>Branch</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {applications.map((row, index) => {
+    return (
+      <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+        <DialogTitle disableTypography>
+          <Typography variant="h6">Selected Application Summary</Typography>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Applicant Name</TableCell>
+                <TableCell>Application Type</TableCell>
+                <TableCell align="right">Approved Amount</TableCell>
+                <TableCell>Account No</TableCell>
+                <TableCell>Bank Name</TableCell>
+                <TableCell>Branch</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {applications.map((row, index) => {
                 const parseBankInfo = JSON.parse(row.employeeBankInfo)
                 const bankInfo = JSON.parse(parseBankInfo)
                 console.log(bankInfo)
-             return   (
-              <TableRow key={index}>
-                <TableCell>{row.workforceEmployee?.firstNameBn}</TableCell>
-                <TableCell>{row.applicationType}</TableCell>
-                <TableCell align="right">20000</TableCell>
-                <TableCell>{bankInfo.accountNumber}</TableCell>
-                <TableCell>{bankInfo?.bank?.nameEn}</TableCell>
-                <TableCell>{bankInfo?.branch?.nameEn}</TableCell>
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{row.workforceEmployee?.firstNameBn}</TableCell>
+                    <TableCell>{row.applicationType}</TableCell>
+                    <TableCell align="right">20000</TableCell>
+                    <TableCell>{bankInfo.accountNumber}</TableCell>
+                    <TableCell>{bankInfo?.bank?.nameEn}</TableCell>
+                    <TableCell>{bankInfo?.branch?.nameEn}</TableCell>
+                  </TableRow>
+                )
+              })}
+              <TableRow>
+                <TableCell colSpan={2}><strong>Total</strong></TableCell>
+                <TableCell align="right"><strong>{getTotalAmount()}</strong></TableCell>
+                <TableCell colSpan={3} />
               </TableRow>
-            )})}
-            <TableRow>
-              <TableCell colSpan={2}><strong>Total</strong></TableCell>
-              <TableCell align="right"><strong>{getTotalAmount()}</strong></TableCell>
-              <TableCell colSpan={3} />
-            </TableRow>
-          </TableBody>
-        </Table>
-      </DialogContent>
-      <Divider />
-      <DialogActions>
-        <Button onClick={onClose} variant="outlined" color="primary">
-          Close
-        </Button>
-        <Button onClick={() => window.print()} variant="contained" color="primary">
-          Print Summary
-        </Button>
-       <Button onClick={() => handleForward()} variant="contained" color="primary">
-        Forward To Director
-        <ForwardIcon />
-      </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}else if(getUserTypeFromRights(userRights) === WORKFORCE_USER_TYPE.ADMIN) {
-   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle disableTypography>
-        <Typography variant="h6">Bank Payment Advice (BFTN)</Typography>
-      </DialogTitle>
-      <DialogContent dividers>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Applicant Name</TableCell>
-              <TableCell>Application Type</TableCell>
-              <TableCell align="right">Approved Amount</TableCell>
-              <TableCell>Account No</TableCell>
-              <TableCell>Bank Name</TableCell>
-              <TableCell>Branch</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {applications.map((row, index) => {
+            </TableBody>
+          </Table>
+        </DialogContent>
+        <Divider />
+        <DialogActions>
+          <Button onClick={onClose} variant="outlined" color="primary">
+            Close
+          </Button>
+          <Button onClick={() => window.print()} variant="contained" color="primary">
+            Print Summary
+          </Button>
+          <Button onClick={() => handleForward()} variant="contained" color="primary">
+            Forward To Director
+            <ForwardIcon />
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  } else if (getUserTypeFromRights(userRights) === WORKFORCE_USER_TYPE.ADMIN) {
+    return (
+      <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+        <DialogTitle disableTypography>
+          <Typography variant="h6">Bank Payment Advice (BFTN)</Typography>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Applicant Name</TableCell>
+                <TableCell>Application Type</TableCell>
+                <TableCell align="right">Approved Amount</TableCell>
+                <TableCell>Account No</TableCell>
+                <TableCell>Bank Name</TableCell>
+                <TableCell>Branch</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {applications.map((row, index) => {
                 const parseBankInfo = JSON.parse(row.employeeBankInfo)
                 const bankInfo = JSON.parse(parseBankInfo)
                 console.log(bankInfo)
-             return   (
-              <TableRow key={index}>
-                <TableCell>{row.workforceEmployee?.firstNameBn}</TableCell>
-                <TableCell>{row.applicationType}</TableCell>
-                <TableCell align="right">20000</TableCell>
-                <TableCell>{bankInfo.accountNumber}</TableCell>
-                <TableCell>{bankInfo?.bank?.nameEn}</TableCell>
-                <TableCell>{bankInfo?.branch?.nameEn}</TableCell>
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{row.workforceEmployee?.firstNameBn}</TableCell>
+                    <TableCell>{row.applicationType}</TableCell>
+                    <TableCell align="right">20000</TableCell>
+                    <TableCell>{bankInfo.accountNumber}</TableCell>
+                    <TableCell>{bankInfo?.bank?.nameEn}</TableCell>
+                    <TableCell>{bankInfo?.branch?.nameEn}</TableCell>
+                  </TableRow>
+                )
+              })}
+              <TableRow>
+                <TableCell colSpan={2}><strong>Total</strong></TableCell>
+                <TableCell align="right"><strong>{getTotalAmount()}</strong></TableCell>
+                <TableCell colSpan={3} />
               </TableRow>
-            )})}
-            <TableRow>
-              <TableCell colSpan={2}><strong>Total</strong></TableCell>
-              <TableCell align="right"><strong>{getTotalAmount()}</strong></TableCell>
-              <TableCell colSpan={3} />
-            </TableRow>
-          </TableBody>
-        </Table>
-      </DialogContent>
-      <Divider />
-      <DialogActions>
-        <Button onClick={onClose} variant="outlined" color="primary">
-          Close
-        </Button>
-        <Button onClick={() => window.print()} variant="contained" color="primary">
-          Print Advice
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
+            </TableBody>
+          </Table>
+        </DialogContent>
+        <Divider />
+        <DialogActions>
+          <Button onClick={onClose} variant="outlined" color="primary">
+            Close
+          </Button>
+          <Button onClick={() => window.print()} variant="contained" color="primary">
+            Print Advice
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 };
 
 export default GenerateBFTN;
