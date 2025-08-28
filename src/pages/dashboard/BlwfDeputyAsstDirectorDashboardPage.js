78434f1a2a0a3bc28156fa50fcc449442474198e
Raduan <raduan.cse@gmail.com>
2025-08-28 11:14:33 +0600

    u

--- src/pages/dashboard/BlwfDeputyAsstDirectorDashboardPage.js
+++ src/pages/dashboard/BlwfDeputyAsstDirectorDashboardPage.js
@@ -24,8 +24,16 @@ import {
   AccordionSummary,
   AccordionDetails,
 } from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { fetchSummaryApplications } from "../../actions";
+import RestorePageIcon from '@material-ui/icons/RestorePage';
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import HourglassFullTwoToneIcon from '@material-ui/icons/HourglassFullTwoTone';
+import CheckCircleOutlineTwoToneIcon from '@material-ui/icons/CheckCircleOutlineTwoTone';
+import DoneAllIcon from '@material-ui/icons/DoneAll';
 import ApplicationProcessSearcher from "../../components/application-process/ApplicationProcessSearcher";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -115,6 +123,7 @@ const SidebarMenu = [
 const FiledApplications = () =>{ 
   const classes = useStyles()
   const loggedInUserId = useSelector((state) => state.core?.user?.i_user?.id);
+
   return (
   <>
     <Typography variant="h5" gutterBottom>
@@ -122,7 +131,7 @@ const FiledApplications = () =>{
     </Typography>
    <Card className={classes.tableContainer}>
         <CardContent>
-            <ApplicationProcessSearcher oggedInUserId={loggedInUserId}
+            <ApplicationProcessSearcher loggedInUserId={loggedInUserId}
                     
             />
           </CardContent>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -53,6 +53,8 @@ export function getUserTypeFromRights(user_rights) {
     user_type = WORKFORCE_USER_TYPE.SEC2_DEPUTI_ASST_DIRECTOR;
   }else if (user_rights.includes(821005)) {
     user_type = WORKFORCE_USER_TYPE.BLWF_SECTION_ADMIN;
+  }else if (user_rights.includes(821007)) {
+    user_type = WORKFORCE_USER_TYPE.BLWF_DEPUTI_ASST_DIRECTOR;
   } else if (user_rights.includes(818001)) {
     user_type = WORKFORCE_USER_TYPE.DOCTOR;
   } else if (user_rights.includes(813001)) {
