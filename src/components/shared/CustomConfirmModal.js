4837c5c96d8a05b310318e64c06b3de297bd670d
Syed Fahim Tazwer <89224568+Taz-war@users.noreply.github.com>
2025-09-25 11:38:46 +0600

    resend modal probably fixed

---
+++ src/components/shared/CustomConfirmModal.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { FormattedMessage,useTranslations,useModulesManager } from "@openimis/fe-core";
+import {
+  Modal,
+  Box,
+  Typography,
+  Button,
+} from "@material-ui/core";
+
+
+
+const CustomConfirmModal = ({ open, message, onClose,onConfirm }) => {
+  const modulesManager = useModulesManager()
+ const { formatMessage } = useTranslations("core.RegistrationPage", modulesManager);
+  return (
+    <Modal open={open} onClose={() => onClose()}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          borderRadius: 2,
+          boxShadow: 24,
+          p: 4,
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          
+          {<FormattedMessage id={`${message}`} module="workforce" /> || "Are you sure?"}
+        </Typography>
+
+        <Box sx={{ mt: 3, display: "flex", justifyContent: "space-around" }}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => onClose()} // cancel
+          >
+            {<FormattedMessage id="workforce.confirm.modal.cancel" module="workforce" /> || "Are you sure?"}
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {onClose();onConfirm()}} // ok
+          >
+            {<FormattedMessage id="workforce.confirm.modal.ok" module="workforce" /> || "Are you sure?"}
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default CustomConfirmModal;
