2d2732030b2b68fff5df2d6234794c576b2067de
Syed Fahim Tazwer <89224568+Taz-war@users.noreply.github.com>
2025-11-03 12:22:33 +0600

    some issue fixed

--- src/components/application-forms/Atoms/DoctorsEntries.js
+++ src/components/application-forms/Atoms/DoctorsEntries.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Grid, FormControlLabel, Radio, RadioGroup, Button } from "@material-ui/core";
 import { FormattedMessage, TextInput } from "@openimis/fe-core";
 import { updateApplication } from "../../../actions";
+import CustomSnackbar from "../../shared/CustomSnackbar";
 
 const DoctorsEntries = ({ application }) => {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ const DoctorsEntries = ({ application }) => {
   const [doctorDiagnosis, setDoctorDiagnosis] = useState("");
   const [doctorComment, setDoctorComment] = useState("");
   const [doctorsActions, setDoctorsActions] = useState("");
+  const [openSnackBar, setOpenSnackBar] = useState({openResponseBar: false,status: "",type:"success"});
 
   const handleSelectCheckbox = (event) => {
     setDoctorsActions(event.target.value);
@@ -19,7 +21,7 @@ const DoctorsEntries = ({ application }) => {
     setDoctorComment("");
   };
 
-  const handleUpdateGrantAmount = () => {
+  const handleUpdateGrantAmount = async () => {
     const updateApplicationData = {
       id: application?.id,
       doctorsRecommendedDonation: proposedAmount,
@@ -27,60 +29,72 @@ const DoctorsEntries = ({ application }) => {
       doctorComment: doctorComment || null,
       doctorsFlag: doctorsActions,
     };
+    try {
+      console.log("Doctor Update Payload:", updateApplicationData);
 
-    console.log("Doctor Update Payload:", updateApplicationData);
-
-    dispatch(updateApplication(updateApplicationData, "update workforce application"));
+      await dispatch(updateApplication(updateApplicationData, "update workforce application"));
+      setOpenSnackBar({openResponseBar:true,status:"আপনার মতামত সফলভাবে সংরক্ষিত করা হয়েছে!",type:"success"})
+    } catch (error) {
+      setOpenSnackBar({openResponseBar:true,status:"আপনার মতামত সংরক্ষণ করা যায়নি।",type:"error"})
+    }
   };
 
   return (
-    <Grid container spacing={2} style={{ marginTop: "10px" }}>
-      {/* Proposed Amount */}
-      <Grid item xs={12}>
-        <TextInput label="workforce.application.proposedAmount.byDoctor" value={proposedAmount || ""} onChange={(v) => setProposedAmount(v)} />
-      </Grid>
-
-      {/* Radio Group */}
-      <Grid item xs={12}>
-        <RadioGroup value={doctorsActions} onChange={handleSelectCheckbox}>
-          <FormControlLabel
-            value="recommend"
-            control={<Radio color="primary" />}
-            label={<FormattedMessage id="workforce.doctor.recommend" module="workforce" />}
-          />
-          <FormControlLabel
-            value="discussion_required"
-            control={<Radio color="primary" />}
-            label={<FormattedMessage id="workforce.doctor.discussionRequired" module="workforce" />}
-          />
-          <FormControlLabel
-            value="reject_request"
-            control={<Radio color="primary" />}
-            label={<FormattedMessage id="workforce.doctor.rejectRequest" module="workforce" />}
-          />
-        </RadioGroup>
-      </Grid>
-
-      {/* Conditional Fields */}
-      {doctorsActions === "recommend" && (
+    <>
+      <Grid container spacing={2} style={{ marginTop: "10px" }}>
+        {/* Proposed Amount */}
         <Grid item xs={12}>
-          <TextInput label="workforce.application.diagnosis.byDoctor" value={doctorDiagnosis} onChange={(v) => setDoctorDiagnosis(v)} />
+          <TextInput label="workforce.application.proposedAmount.byDoctor" value={proposedAmount || ""} onChange={(v) => setProposedAmount(v)} />
         </Grid>
-      )}
 
-      {(doctorsActions === "discussion_required" || doctorsActions === "reject_request") && (
+        {/* Radio Group */}
         <Grid item xs={12}>
-          <TextInput label="workforce.application.reasons.addComment" value={doctorComment} onChange={(v) => setDoctorComment(v)} multiline rows={3} />
+          <RadioGroup value={doctorsActions} onChange={handleSelectCheckbox}>
+            <FormControlLabel
+              value="recommend"
+              control={<Radio color="primary" />}
+              label={<FormattedMessage id="workforce.doctor.recommend" module="workforce" />}
+            />
+            <FormControlLabel
+              value="discussion_required"
+              control={<Radio color="primary" />}
+              label={<FormattedMessage id="workforce.doctor.discussionRequired" module="workforce" />}
+            />
+            <FormControlLabel
+              value="reject_request"
+              control={<Radio color="primary" />}
+              label={<FormattedMessage id="workforce.doctor.rejectRequest" module="workforce" />}
+            />
+          </RadioGroup>
         </Grid>
-      )}
 
-      {/* Submit Button */}
-      <Grid item xs={3}>
-        <Button variant="contained" color="primary" onClick={handleUpdateGrantAmount} disabled={!doctorsActions || !proposedAmount}>
-          <FormattedMessage id="workforce.submit" module="workforce" />
-        </Button>
+        {/* Conditional Fields */}
+        {doctorsActions === "recommend" && (
+          <Grid item xs={12}>
+            <TextInput label="workforce.application.diagnosis.byDoctor" value={doctorDiagnosis} onChange={(v) => setDoctorDiagnosis(v)} />
+          </Grid>
+        )}
+
+        {(doctorsActions === "discussion_required" || doctorsActions === "reject_request") && (
+          <Grid item xs={12}>
+            <TextInput label="workforce.application.reasons.addComment" value={doctorComment} onChange={(v) => setDoctorComment(v)} multiline rows={3} />
+          </Grid>
+        )}
+
+        {/* Submit Button */}
+        <Grid item xs={3}>
+          <Button variant="contained" color="primary" onClick={handleUpdateGrantAmount} disabled={!doctorsActions || !proposedAmount}>
+            <FormattedMessage id="workforce.submit" module="workforce" />
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
+      <CustomSnackbar
+        open={openSnackBar}
+        onClose={() => setOpenSnackBar(false)}
+        type={openSnackBar?.type}
+        message={<FormattedMessage id={openSnackBar?.status} module="workforce" />}
+      />
+    </>
   );
 };
 
