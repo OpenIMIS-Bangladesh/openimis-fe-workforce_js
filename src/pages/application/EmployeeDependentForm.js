d4d20c3a9ca5dac9ea312714fbf352c334222fbe
Syed Fahim Tazwer <89224568+Taz-war@users.noreply.github.com>
2026-01-21 16:23:48 +0600

    selection committee problem fixed

--- src/pages/application/EmployeeDependentForm.js
+++ src/pages/application/EmployeeDependentForm.js
@@ -79,7 +79,7 @@ const EmployeeDependentForm = ({ applicationType, dependents, handleChange, addI
   const getRelationAwareLabel = useCallback(
     (dependent, labelKey) => {
       return applicationType === "financialAssistance" && dependent?.relationType
-        ? `${formatMessage(dependent.relationType)}${locale==="en"?"'s":"র"} ${formatMessage(labelKey)}`
+        ? `${formatMessage(dependent.relationType)}${formatMessage("worforce.dependent.suffix")} ${formatMessage(labelKey)}`
         : formatMessage(labelKey);
     },
     [applicationType, formatMessage]
