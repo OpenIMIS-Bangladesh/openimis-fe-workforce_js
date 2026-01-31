2f447504e1bb2ea5890d067bc9a193cd69b23076
Tahir082 <tahir.ewu@gmail.com>
2026-01-31 12:07:50 +0600

    utils push

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -998,6 +998,6 @@ export const isEisPath = () => {
   if (typeof window !== "undefined") {
     return window.location.href.includes("eis");
   }
-  return false; // fallback if window is not defined (SSR)
+  return false; // fallback if window is not defined
   // return true;
 };
