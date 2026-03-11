552cdb869a033892d73854627b227d4cf0c416e1
Raduan <raduan.cse@gmail.com>
2026-03-11 15:55:51 +0600

    death date for approval note

--- src/pages/application-process/EisApprovalSignature.js
+++ src/pages/application-process/EisApprovalSignature.js
@@ -878,7 +878,7 @@ const EisApprovalSignature = ({ open, onClose, userRights, selectedApplicationId
                 )}
                 <tr>
                   <td style={{ fontWeight: "bold" }}>Effective date of Benefit</td>
-                  <td>{dateOfRejoining || dateOfAssessment || ""}</td>
+                  <td>{parsedAccidentInfo?.dateOfDeath || ""}</td>
                   <td style={{ border: "none" }}></td>
                   <td style={{ fontWeight: "bold" }}>Type of Accident</td>
                   <td>
