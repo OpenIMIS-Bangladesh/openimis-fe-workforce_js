a5ea66aa0bec6665f9ddc2373332b24d062a7b5a
Tahir082 <tahir.ewu@gmail.com>
2026-03-29 13:07:23 +0600

    done

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1410,9 +1410,9 @@ export const fixBrokenUnicode = (text) => {
 
   
 export const isEisPath = () => {
-  // if (typeof window !== "undefined") {
-  //   return window.location.href.includes("eis");
-  // }
-  // return false; // fallback if window is not defined
-  return true;
+  if (typeof window !== "undefined") {
+    return window.location.href.includes("eis");
+  }
+  return false; // fallback if window is not defined
+  // return true;
 };
