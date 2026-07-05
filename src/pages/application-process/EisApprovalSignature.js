5cd22708c7a2b3e24c00cb4691ed2905527ec501
Syed Fahim Tazwer <fahimtazwer@gmail.com>
2026-07-05 09:39:55 +0600

    resolved eis approval signature format

--- src/pages/application-process/EisApprovalSignature.js
+++ src/pages/application-process/EisApprovalSignature.js
@@ -1085,6 +1085,8 @@ const EisApprovalSignature = ({ open, onClose, userRights, selectedApplicationId
                     {/* Name and Role */}
                     <div className={classes.signatureBlock}>
                       <p style={{ margin: 0, fontWeight: "bold" }}>{sig?.designation}</p>
+                      <p style={{ margin: 0, fontWeight: "bold" }}>{sig?.organization_name}</p>
+                      <p style={{ margin: 0, fontWeight: "bold" }}>{sig?.representative_type}</p>
                       <p style={{ margin: 0 }}>{sig?.role?.name}</p>
                     </div>
                   </Grid>
