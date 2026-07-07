d667ccd1178a440792d9741bd96eedaf8a553c38
Tahir082 <tahir.ewu@gmail.com>
2026-07-07 15:46:17 +0600

    committee report fix

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1563,11 +1563,11 @@ export const formatAddress = (locationData, addressData) => {
   };
 
 export const isCfPath = () => {
-  if (typeof window !== "undefined") {
-    return window.location.href.includes("cf");
-  }
-  return false; // fallback if window is not defined
-  // return true;
+  // if (typeof window !== "undefined") {
+  //   return window.location.href.includes("cf");
+  // }
+  // return false; // fallback if window is not defined
+  return true;
 };
 
 export const isBlwfPath = () => {
